7c6e8db9acbbcce5e3f06dfd06e205ba1f548811
avagadro <fragarie@yandex.com>
2022-03-22 22:26:05 +0300

    Coloring page: fill inner contours
    all calculations are client-side

--- static/js/coloring.js
+++ static/js/coloring.js
@@ -1,6 +1,7 @@
 // add on document load listener
 window.addEventListener('load', function () {
     const canvas = document.getElementById('canvas');
+    const source = new Image();
     let contours = null;
     let hierarchy = null;
 
@@ -8,25 +9,45 @@ window.addEventListener('load', function () {
     cv.onRuntimeInitialized = function () {
         const fromIndex = window.location.href.lastIndexOf('/') + 1;
         socket.emit('get_coloring_data', window.location.href.substring(fromIndex), (data) => {
-            const imageURL = data['source'];
             const filters = data['filters'];
-            drawPicture(canvas, imageURL, filters)
-                .then((value) => {
-                    [contours, hierarchy] = value;
-                    loadingIndicator.hidden = true;
-                })
+            source.onload = function () {
+                drawPicture(canvas, source, filters)
+                    .then((value) => {
+                        [contours, hierarchy] = value;
+                        loadingIndicator.hidden = true;
+                    })
+            }
+            source.src = data['source'];
         })
+
+        // on context click event
+        canvas.onclick = function (ev) {
+            console.log('canvas onclick caught', ev.offsetX, ':', ev.offsetY);
+            // detect clicked area
+            if (!contours || !hierarchy) {
+                alert('Critical error!');
+                return
+            }
+
+            // Note that the designations "inner" and "outer" are used here in relation to the contour, not to the point
+            // let outerDist, innerDist = null;
+            // let outerIndex, innerIndex = null;
+            let outerContoursIndex = [];
+            for (let i = 0; i < contours.size(); ++i) {
+                const ci = contours.get(i);
+                // let dist = cv.pointPolygonTest(ci, new cv.Point(ev.offsetX, ev.offsetY), true);
+                const dist = cv.pointPolygonTest(ci, new cv.Point(ev.offsetX, ev.offsetY), false);
+                if (dist > 0)
+                    outerContoursIndex.push(i);
+            }
+            // search contours at the same hierarchy level
+            // TODO ...
+
+            console.log(outerContoursIndex)
+
+            // fill clicked contour
+            // TODO how to pass to func outer/inner contours?
+            fillContour(canvas, source, contours.get(outerContoursIndex[outerContoursIndex.length - 1]));
+        }
     }
-
-
-
-    // on context click event
-    // canvas.onclick = function (ev) {
-    //     console.log('canvas onclick caught', ev.offsetX, ':', ev.offsetY);
-    //     socket.emit('click', [ev.offsetX, ev.offsetY], function (points) {
-    //         console.log('contour points:', points);
-    //         drawContour(context, points, 'static/media/testpic.jpg')
-    //     });
-    // }
-    // drawPicture(context, 'static/media/testpic_contours.jpg');
 });
--- static/js/drawing.js
+++ static/js/drawing.js
@@ -1,78 +1,67 @@
-function drawPicture(canvas, sourcePath, filters) {
+function drawPicture(canvas, sourceImage, filters) {
     return new Promise((resolve, reject) => {
-        let image = new Image();
-        image.onload = function () {
-            // TODO: add pre-scaling if size is more than required
-            canvas.width = image.width;
-            canvas.height = image.height;
-            canvas.getContext('2d').drawImage(image, 0, 0);
+        // TODO: add pre-scaling if size is more than required
+        canvas.width = sourceImage.width;
+        canvas.height = sourceImage.height;
+        canvas.getContext('2d').drawImage(sourceImage, 0, 0);
+        // apply filters
+        if (filters !== undefined) {
+            // parse parameters
+            let contoursOnly = filters['checkBlankBack'];
+
+            // init images
+            let originalImage = cv.imread(canvas);
+            let contoursImage = new cv.Mat(originalImage.rows, originalImage.cols, originalImage.type(), [255,255,255,255]);
+            let filteredImage = new cv.Mat();
+            // parse filters
+            let blur = filters['rangeBlur'];
+            let low = [filters['rangeB1'], filters['rangeG1'], filters['rangeR1'], 0]
+            let high = [filters['rangeB2'], filters['rangeG2'], filters['rangeR2'], 255]
+            low = new cv.Mat(originalImage.rows, originalImage.cols, originalImage.type(), low);
+            high = new cv.Mat(originalImage.rows, originalImage.cols, originalImage.type(), high);
             // apply filters
-            if (filters !== undefined) {
-                // parse parameters
-                let contoursOnly = filters['checkBlankBack'];
+            cv.blur(originalImage, filteredImage, new cv.Size(blur, blur));
+            cv.inRange(filteredImage, low, high, filteredImage);
+            // cv.cvtColor(originalImage, filteredImage, cv.COLOR_RGBA2GRAY, 0);
+            low.delete();
+            high.delete();
+            // TODO: how to make `master` contour?
 
-                // init images
-                let originalImage = cv.imread(canvas);
-                let contoursImage = new cv.Mat(originalImage.rows, originalImage.cols, originalImage.type(), [255,255,255,255]);
-                let filteredImage = new cv.Mat();
-                // parse filters
-                let blur = filters['rangeBlur'];
-                let low = [filters['rangeB1'], filters['rangeG1'], filters['rangeR1'], 0]
-                let high = [filters['rangeB2'], filters['rangeG2'], filters['rangeR2'], 255]
-                low = new cv.Mat(originalImage.rows, originalImage.cols, originalImage.type(), low);
-                high = new cv.Mat(originalImage.rows, originalImage.cols, originalImage.type(), high);
-                // apply filters
-                cv.blur(originalImage, filteredImage, new cv.Size(blur, blur));
-                cv.inRange(filteredImage, low, high, filteredImage);
-                // cv.cvtColor(originalImage, filteredImage, cv.COLOR_RGBA2GRAY, 0);
-                low.delete();
-                high.delete();
-                // TODO: how to make `master` contour?
-
-                // find contours and draw
-                let contours = new cv.MatVector();
-                let hierarchy = new cv.Mat();
-                let color = new cv.Scalar(0, 0, 255, 255);       // TODO: add contour color selector
-                cv.findContours(filteredImage, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
-                for (let i = 0; i < contours.size(); i++) {
-                    cv.drawContours(contoursOnly ? contoursImage : originalImage, contours, i, color, 1, cv.LINE_AA, hierarchy, 100);
-                }
-                contours.delete();
-                hierarchy.delete();
-                cv.imshow(canvas, contoursOnly ? contoursImage : originalImage);
-
-                // resolve
-                // let filteredCanvas = document.createElement('canvas');
-                // cv.imshow(filteredCanvas, filteredImage);
-                // resolve(filteredCanvas.toDataURL('image/png'));
-                resolve([contours, hierarchy])
-                // delete images
-                originalImage.delete();
-                filteredImage.delete();
-                contoursImage.delete();
-            } else {
-                reject();
+            // find contours and draw
+            let contours = new cv.MatVector();
+            let hierarchy = new cv.Mat();
+            let color = new cv.Scalar(0, 0, 255, 255);       // TODO: add contour color selector
+            cv.findContours(filteredImage, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE);
+            for (let i = 0; i < contours.size(); i++) {
+                cv.drawContours(contoursOnly ? contoursImage : originalImage, contours, i, color, 1, cv.LINE_AA, hierarchy, 100);
             }
+            cv.imshow(canvas, contoursOnly ? contoursImage : originalImage);
+
+            // resolve
+            resolve([contours, hierarchy])
+            // delete images
+            originalImage.delete();
+            filteredImage.delete();
+            contoursImage.delete();
+        } else {
+            reject();
         }
-        image.src = sourcePath;
     })
 }
 
-// function drawContour(ctx, points, sourcePath) {
-//     let image = new Image();
-//     image.src = sourcePath;
-//     image.onload = function () {
-//         let pattern = ctx.createPattern(image, 'repeat');
-//         ctx.save();
-//         ctx.beginPath();
-//         ctx.fillStyle = pattern;
-//         ctx.moveTo(points[0][0][0], points[0][0][1])
-//         for (let i = 1; i < points.length; i++) {
-//             ctx.lineTo(points[i][0][0], points[i][0][1]);
-//         }
-//         ctx.lineTo(points[0][0][0], points[0][0][1]);
-//         ctx.closePath();
-//         ctx.fill();
-//         ctx.restore();
-//     }
-// }
+function fillContour(canvas, sourceImage, contour) {
+    let context = canvas.getContext('2d');
+    let pattern = context.createPattern(sourceImage, 'repeat');
+    context.save();
+    context.beginPath();
+    context.fillStyle = pattern;
+
+    context.moveTo(contour.data32S[0], contour.data32S[1]);
+    for (let i = 2; i < contour.data32S.length; i += 2) {
+        context.lineTo(contour.data32S[i], contour.data32S[i + 1])
+    }
+
+    context.closePath();
+    context.fill();
+    context.restore();
+}
